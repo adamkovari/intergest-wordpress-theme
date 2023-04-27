3c72caa13dae4d1f535778e8557ec504ded0169e
adamkovari <kovari.adam@szmacscs.hu>
2023-04-27 04:16:36 +0200

    migration

--- footer.php
+++ footer.php
@@ -10,9 +10,9 @@
 <footer id="colophon" class="site-footer py-12" role="contentinfo">
 	<?php do_action( 'intergest_theme_footer' ); ?>
 
-	<div class="w-11/12 mx-auto flex flex-col-reverse md:flex-row">
+	<div class="w-fit mx-auto flex flex-col-reverse lg:flex-row">
 
-		<div class="w-full md:w-2/5 md:px-14">
+		<div class="mx-auto md:w-2/5 md:px-14 lg:mt-10 flex flex-col items-center">
 
 			<div class="flex gap-x-4 w-2/5 md:w-3/5 lg:w-2/5 mt-10 items-center md:mx-auto justify-around">
 
@@ -42,7 +42,7 @@
 
 			</div>
 
-			<div class="w-3/5 md:w-4/5 lg:w-3/5 md:mx-auto mt-10">
+			<div class="w-3/5 md:w-4/5 xl:w-3/5 md:mx-auto mt-10">
 				<?php if ( has_custom_logo() ) { ?>
 					<?php the_custom_logo(); ?>
 				<?php } else { ?>
@@ -56,11 +56,13 @@
 
 				<?php } ?>
 			</div>
+
 		</div>
 
 
-		<div class="w-full md:w-3/5 block md:flex md:justify-around">
-			<div class="w-fit md:w-1/3">
+		<div class="w-4/5 ml-24 flex flex-col sm:flex-row sm:gap-x-10 items-start sm:mx-auto">
+
+			<div class="w-fit mt-5 md:w-1/3">
 
 				<p class="uppercase text-text2 font-extrabold text-2xl">Intergest</p>
 
@@ -69,17 +71,18 @@
 						wp_nav_menu(
 							array(
 								'container_id'    => 'primary-menu',
-								'container_class' => 'mt-4 p-4 lg:mt-0 lg:p-0 lg:bg-transparent',
-								'menu_class'      => '-mx-3',
+								'container_class' => '',
+								'menu_class'      => '',
 								'theme_location'  => 'primary',
-								'li_class'        => 'lg:mx-3 text-base hover:text-primary hover:font-semibold',
+								'li_class'        => 'text-base hover:text-primary hover:font-semibold',
 								'fallback_cb'     => false,
 							)
 						);
 						?>
 				</div>
 			</div>
-			<div class="w-fit md:w-1/3">
+
+			<div class="w-fit mt-5 md:w-1/3">
 
 				<p class="uppercase text-text2 font-extrabold text-2xl">
 					<?php echo wp_get_nav_menu_name('services') ?>
@@ -91,10 +94,10 @@
 						array(
 							'menu' => 'services',
 							'container_id'    => 'footer-services',
-							'container_class' => 'flex mt-4 p-4 lg:mt-0 lg:p-0 lg:bg-transparent',
-							'menu_class'      => 'block gap-x-4 -mx-3',
+							'container_class' => 'flex',
+							'menu_class'      => '',
 							'theme_location'  => 'services',
-							'li_class'        => 'lg:mx-3 text-base hover:text-primary',
+							'li_class'        => 'text-base hover:text-primary',
 							'fallback_cb'     => false,
 						)
 					);
@@ -102,7 +105,8 @@
 				</div>
 
 			</div>
-			<div class="w-fit md:w-1/3">
+
+			<div class="w-fit mt-5 md:w-1/3">
 				<p class="uppercase text-text2 font-extrabold text-2xl">
 					<?php echo wp_get_nav_menu_name('contact') ?>
 				</p>
@@ -146,15 +150,16 @@
 				</div>
 			</div>
 		</div>
+
 	</div>
-	<div class="md:w-11/12 mt-14 mx-auto flex justify-between">
-		<div class="flex flex-col md:mx-0">
-			<hr class="border-text1 border-[1px]" />
+
+	<div class="w-fit mt-14 mx-auto">
+		<hr class="border-text1 border-[1px]" />
 			<?php
 				wp_nav_menu(
 					array(
 						'menu' => 'footer',
-						'container_id'    => 'footer-nav',
+						'container_id'    => 'footer-privacy',
 						'container_class' => 'flex mt-4 p-4 lg:mt-0 lg:p-0 lg:bg-transparent',
 						'menu_class'      => 'block md:flex gap-x-4 mt-3 lg:-mx-4',
 						'theme_location'  => 'primary',
@@ -163,13 +168,8 @@
 					)
 				);
 				?>
-		</div>
-		<!--
-		<div class="">
-			&copy; <?php echo date_i18n( 'Y' );?> - <?php echo get_bloginfo( 'name' );?>
-		</div>
-		-->
 	</div>
+
 </footer>
 
 </div>
